6a7698dfa6613beea9438f46908998a08281c855
rakib4kbd <rakib4kbd@gmail.com>
2022-09-09 16:16:38 +0600

    init_org_runner

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 ## Instructions
 
-1. Populate the below inside docker-compose.yml file with the actual values  
+
+1. Login to Organization Admin User
+
+2. Create an __access_token__ for the account and provide __admin:org__ access
+
+3. Populate the below inside docker-compose.yml file with the actual values  
     ```
-    GITHUB_USERNAME=rickdonato  
-    GITHUB_REPO=docker-gh-runner  
+    GITHUB_ORGNAME=packetcoders  
     ACCESS_TOKEN=github-access-token  
     ```
 
-2. Run docker-compose yml file
+4. Run docker-compose yml file
     ```
     docker-compose up --build
     ```
--- start.sh
+++ start.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-GITHUB_USERNAME=$GITHUB_USERNAME
-GITHUB_REPO=$GITHUB_REPO
+GITHUB_ORGNAME=$GITHUB_ORGNAME
 ACCESS_TOKEN=$ACCESS_TOKEN
 
-REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/repos/${GITHUB_USERNAME}/${GITHUB_REPO}/actions/runners/registration-token | jq .token --raw-output)
+REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${ACCESS_TOKEN}" https://api.github.com/orgs/${GITHUB_ORGNAME}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
 
-./config.sh --url https://github.com/${GITHUB_USERNAME}/${GITHUB_REPO} --token ${REG_TOKEN}
+./config.sh --url https://github.com/${GITHUB_ORGNAME} --token ${REG_TOKEN}
 
 cleanup() {
     echo "Removing runner..."
