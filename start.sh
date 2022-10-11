6e47f03a97f76ca724e8c95ea0e960fc6a61ae1b
Rick Donato <rick.donato@packetcoders.io>
2022-10-11 21:09:06 +0100

    init_org_runner (#2)
    * init_org_runner
    
    * personal or org functionality
    
    Co-authored-by: rakib4kbd <rakib4kbd@gmail.com>

--- start.sh
+++ start.sh
@@ -1,22 +1,54 @@
 #!/bin/bash
 
+GITHUB_ORGNAME=$GITHUB_ORGNAME
 GITHUB_USERNAME=$GITHUB_USERNAME
 GITHUB_REPO=$GITHUB_REPO
 ACCESS_TOKEN=$ACCESS_TOKEN
 
-REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/repos/${GITHUB_USERNAME}/${GITHUB_REPO}/actions/runners/registration-token | jq .token --raw-output)
 
-cd /home/docker/actions-runner
+organization_profile_registration () {
 
-./config.sh --url https://github.com/${GITHUB_USERNAME}/${GITHUB_REPO} --token ${REG_TOKEN}
+    REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${ACCESS_TOKEN}" https://api.github.com/orgs/${GITHUB_ORGNAME}/actions/runners/registration-token | jq .token --raw-output)
 
-cleanup() {
-    echo "Removing runner..."
-    ./config.sh remove --unattended --token ${REG_TOKEN}
+    cd /home/docker/actions-runner
+
+    ./config.sh --url https://github.com/${GITHUB_ORGNAME} --token ${REG_TOKEN}
+
+    cleanup() {
+        echo "Removing runner..."
+        ./config.sh remove --unattended --token ${REG_TOKEN}
+    }
+
+    trap 'cleanup; exit 130' INT
+    trap 'cleanup; exit 143' TERM
+
+    ./run.sh & wait $!
+
 }
 
-trap 'cleanup; exit 130' INT
-trap 'cleanup; exit 143' TERM
+personal_profile_registration () {
 
-./run.sh & wait $!
+    REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${ACCESS_TOKEN}" https://api.github.com/repos/${GITHUB_USERNAME}/${GITHUB_REPO}/actions/runners/registration-token | jq .token --raw-output)
 
+    cd /home/docker/actions-runner
+
+    ./config.sh --url https://github.com/${GITHUB_USERNAME}/${GITHUB_REPO} --token ${REG_TOKEN}
+
+    cleanup() {
+        echo "Removing runner..."
+        ./config.sh remove --unattended --token ${REG_TOKEN}
+    }
+
+    trap 'cleanup; exit 130' INT
+    trap 'cleanup; exit 143' TERM
+
+    ./run.sh & wait $!
+}
+
+
+if [[ -z "$GITHUB_ORGNAME" ]]
+then
+    personal_profile_registration
+else
+    organization_profile_registration
+fi
